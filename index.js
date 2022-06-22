776a7cb3dfec01b740208e3461445d8f51ef4ab6
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2022-06-22 15:06:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// javascript assignment to print name ,height and country
+const FirstName = 'oniyide' ;
+const  lastName= ' faith';
+const myName = FirstName + lastName
+let height = 5.5 //in feet
+let country = 'Nigeria'
+
+console.log(myName)
+console.log(height)
+console.log(country)
